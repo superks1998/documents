ba3ba9081a01e7e58aa466a7f42a9fcfd1e0dbdc
superks1998 <superks1998@gmail.com>
2024-09-02 23:25:11 +0700

    fix bug deploy

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -29,10 +29,6 @@ const config = {
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
-  i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
-  },
 
   presets: [
     [
